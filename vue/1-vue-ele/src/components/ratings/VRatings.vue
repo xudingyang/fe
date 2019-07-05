19c3f7a9df160a3eb938065e2e960ea52f3985ed
Ricky <xdymail@163.com>
2019-07-05 22:26:36 +0800

    20190705

--- vue/1-vue-ele/src/components/ratings/VRatings.vue
+++ vue/1-vue-ele/src/components/ratings/VRatings.vue
@@ -1,28 +1,342 @@
 <template>
-  <div class="VRatings">
-    rating
+  <div class="ratings" ref="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star class="star" :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star class="star" :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+      <ratingCtl class="rating-ctl" :ratings="ratings" :selectType="selectType" :onlyContent="onlyContent" :desc="desc"
+                 @select="selectRating" @toggle="toggleContent"></ratingCtl>
+      <div class="rating-wrapper">
+        <ul>
+          <li class="reting-item" v-for="(rating, index) in ratings" :key="index" v-show="showRating(rating.rateType,rating.text)">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar">
+            </div>
+            <div class="content">
+              <div class="name">{{rating.username}}</div>
+              <div class="score-star">
+                <star class="star" :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon"
+                      :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>
+                <span class="item" v-for="(item, index) in rating.recommend" :key="index">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import star from '../star/star.vue'
+  import ratingCtl from '../ratingctl/ratingctl.vue'
+  import { formatDate } from '../../common/js/date'
+  import BScroll from 'better-scroll'
+
+  const ALL = 2
+  const ERR_OK = 0
+
   export default {
     name: 'ratings',
-
-    components: {},
-
-    data () {
-      return {}
+    props: {
+      seller: {
+        type: Object
+      }
     },
-
-    computed: {},
-
-    created () {},
-
-    methods: {}
+    data () {
+      return {
+        ratings: [],
+        selectType: ALL,
+        onlyContent: false,
+        desc: {
+          all: '全部',
+          positive: '满意',
+          negative: '不满意'
+        }
+      }
+    },
+    created () {
+      this.$http.get('api/ratings').then((res) => {
+        let response = res.data
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$refs.ratings, {
+              click: true
+            })
+          })
+        }
+      })
+    },
+    methods: {
+      selectRating (selectType) {
+        this.selectType = selectType
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      },
+      toggleContent () {
+        this.onlyContent = !this.onlyContent
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      },
+      showRating (type, text) {
+        if (this.onlyContent && !text) {
+          return false
+        }
+        if (this.selectType === ALL) {
+          return true
+        } else {
+          return this.selectType === type
+        }
+      }
+    },
+    filters: {
+      formatDate (time) {
+        let date = new Date(time)
+        return formatDate(date, 'yyyy-MM-dd hh:mm')
+      }
+    },
+    components: {
+      star,
+      ratingCtl
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-  .VRatings {
+  @import '../../common/scss/mixin';
+
+  .ratings {
+    position: absolute;
+    top: 174px;
+    bottom: 0;
+    width: 100%;
+    overflow: hidden;
+    background-color: #f3f5f7;
   }
+
+  .overview {
+    background-color: #fff;
+    display: flex;
+    padding: 18px 0;
+    border-bottom: 0 solid rgba(7, 17, 27, 0.1);
+    /*.border-w(0, 0, 1px, 0);*/
+    @include border-bottom-1px();
+    margin-bottom: 18px;
+  }
+
+  .overview-left {
+    flex: 0 0 137px;
+    padding: 6px 0;
+    width: 137px;
+    border-right: 1px solid rgba(7, 17, 27, 0.1);
+    text-align: center;
+    // 设计稿没有考虑到5s小屏幕的情况，这里考虑一下
+    @media only screen and (max-width: 320px) {
+      flex: 0 0 120px;
+      width: 120px;
+    }
+
+    .score {
+      margin-bottom: 6px;
+      line-height: 28px;
+      font-size: 24px;
+      color: rgb(255, 153, 0);
+    }
+
+    .title {
+      margin-bottom: 8px;
+      line-height: 12px;
+      font-size: 12px;
+      color: rgb(7, 17, 27);
+    }
+
+    .rank {
+      line-height: 10px;
+      font-size: 10px;
+      color: rgb(147, 153, 159);
+    }
+  }
+
+  .overview-right {
+    flex: 1;
+    padding: 0 24px;
+    @media only screen and (max-width: 320px) {
+      padding: 0 4px;
+    }
+
+    .score-wrapper {
+      margin-bottom: 8px;
+      font-size: 0;
+
+      .score {
+        display: inline-block;
+        line-height: 18px;
+        vertical-align: top;
+        font-size: 12px;
+        color: rgb(255, 153, 0);
+      }
+    }
+
+    .delivery-wrapper {
+      font-size: 0;
+
+      .delivery {
+        display: inline-block;
+        font-size: 12px;
+        line-height: 18px;
+        margin-left: 12px;
+        color: rgb(147, 153, 159);
+      }
+    }
+
+    .title {
+      display: inline-block;
+      font-size: 12px;
+      line-height: 18px;
+      color: rgb(7, 17, 27);
+    }
+
+    .star {
+      display: inline-block;
+      margin: 0 12px;
+      vertical-align: top;
+    }
+  }
+
+  .rating-ctl {
+    background-color: #fff;
+  }
+
+  .rating-wrapper {
+    background-color: #fff;
+    padding: 0 18px;
+  }
+
+  .reting-item {
+    display: flex;
+    padding: 18px 0;
+    border-bottom: 0 solid rgba(7, 17, 27, .1);
+    /*.border-w(0, 0, 1px, 0);*/
+    @include border-bottom-1px();
+
+    .avatar {
+      flex: 0 0 28px;
+      width: 28px;
+      margin-right: 12px;
+
+      img {
+        border-radius: 50%;
+      }
+    }
+
+    .content {
+      position: relative;
+      flex: 1;
+    }
+
+    .name {
+      font-size: 10px;
+      line-height: 12px;
+      color: rgb(7, 17, 27);
+      margin-bottom: 4px;
+    }
+
+    .score-star {
+      margin: 0 6px 6px 0;
+      font-size: 0;
+
+      .star {
+        display: inline-block;
+        margin-right: 6px;
+        vertical-align: top;
+      }
+
+      .delivery {
+        display: inline-block;
+        font-size: 10px;
+        line-height: 12px;
+        color: rgb(147, 153, 159);
+        vertical-align: top;
+      }
+    }
+
+    .text {
+      line-height: 18px;
+      color: rgb(7, 17, 27);
+      font-size: 12px;
+    }
+
+    .recommend {
+      line-height: 16px;
+      font-size: 0;
+      margin-top: 8px;
+
+      .icon {
+        display: inline-block;
+        margin-right: 8px;
+        font-size: 12px;
+      }
+
+      .icon-thumb_up {
+        color: rgb(0, 160, 220);
+      }
+
+      .icon-thumb_down {
+        color: rgb(147, 153, 159);
+      }
+
+      .item {
+        display: inline-block;
+        font-size: 9px;
+        margin-right: 8px;
+        padding: 0 6px;
+        border: 0 solid rgba(7, 17, 27, .1);
+        /*.border-w(1px, 1px, 1px, 1px);*/
+        @include border-bottom-1px();
+        border-radius: 1px;
+        color: rgb(147, 153, 159);
+      }
+    }
+
+    .time {
+      position: absolute;
+      top: 0;
+      right: 0;
+      line-height: 12px;
+      font-size: 10px;
+      color: rgb(147, 153, 159);
+    }
+  }
+
 </style>
--- vue/1-vue-ele/src/components/seller/VSeller.vue
+++ vue/1-vue-ele/src/components/seller/VSeller.vue
@@ -1,28 +1,400 @@
 <template>
-  <div class="VSeller">
-    seller
+  <div class="seller" ref="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <div class="desc">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="rating">
+            <star class="star" :size="36" :score="seller.score"></star>
+            <span class="rating-count">({{seller.ratingCount}})</span>
+            <span class="sell-count">月售{{seller.sellCount}}单</span>
+          </div>
+        </div>
+        <div class="delivery">
+          <div class="delivery-item">
+            <h1>起送价</h1>
+            <span>{{seller.minPrice}}</span><i>元</i>
+          </div>
+          <div class="delivery-item">
+            <h1>商家配送</h1>
+            <span>{{seller.deliveryPrice}}</span><i>元</i>
+          </div>
+          <div class="delivery-item">
+            <h1>平均配送时间</h1>
+            <span>{{seller.deliveryTime}}</span><i>分钟</i>
+          </div>
+        </div>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active':favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
+      </div>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <p class="content">{{seller.bulletin}}</p>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item" v-for="(item, index) in seller.supports" :key="index">
+            <span class="icon" :class="iconClassMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li class="pic-item" v-for="(item, index) in seller.pics" :key="index">
+              <img :src="item" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="info">
+        <h1 class="title">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="(info, index) in seller.infos" :key="index">{{info}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import star from '../star/star.vue'
+  import BScroll from 'better-scroll'
+  import { saveToLocal, loadFromLocal } from '../../common/js/store'
+
   export default {
-    name: 'VSeller',
-
-    components: {},
-
-    data () {
-      return {}
+    name: 'seller',
+    props: {
+      seller: {
+        type: Object
+      }
     },
-
-    computed: {},
-
-    created () {},
-
-    methods: {}
+    data () {
+      return {
+        // 其实iconClassMap不用在data里声明，created里已经有了this.iconClassMap
+//        iconClassMap: [],
+        favorite: (() => {
+          return loadFromLocal(this.seller.id, 'favorite', false)
+        })()
+      }
+    },
+    computed: {
+      favoriteText () {
+        return this.favorite ? '已收藏' : '收藏'
+      }
+    },
+    created () {
+      this.iconClassMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+    },
+    mounted () {
+      // 既然在watch观察了seller，那么为什么在这里还要搞一次呢？
+      // 因为从本组件切到别的组件、再切回本组件的时候，seller是不会变化的，所以watch里的东东就不起作用了
+      this.$nextTick(() => {
+        this._initScroll()
+        this._initPics()
+      })
+    },
+    watch: {
+      // 要确保seller有数据了才增加BScroll事件
+      // seller是其他组件传过来的，当本组件created的时候不能保证有seller，所以在created的$nextTick方法里也不能设置BScroll
+      // 其他组件在created里通过网络请求获得数据，所以可以再created的$nextTick里面增加BScroll。但是本组件不是这样。
+      'seller' () {
+        this.$nextTick(() => {
+          this._initScroll()
+          this._initPics()
+        })
+      }
+    },
+    methods: {
+      toggleFavorite (event) {
+        if (!event._constructed) {
+          return
+        }
+        this.favorite = !this.favorite
+        saveToLocal(this.seller.id, 'favorite', this.favorite)
+      },
+      _initScroll () {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.seller, {
+            click: true
+          })
+        } else {
+          this.scroll.refresh()
+        }
+      },
+      _initPics () {
+        if (this.seller.pics) {
+          let picWidth = 120
+          let margin = 6
+          let width = (picWidth + margin) * this.seller.pics.length - margin
+          this.$refs.picList.style.width = width + 'px'
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$refs.picWrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              })
+            } else {
+              this.picScroll.refresh()
+            }
+          })
+        }
+      }
+    },
+    components: {
+      star
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-  .VSeller {
+  @import "../../common/scss/mixin";
+
+  .seller {
+    position: absolute;
+    top: 174px;
+    bottom: 0;
+    width: 100%;
+    overflow: hidden;
+    background-color: #f3f5f7;
+  }
+
+  .overview {
+    position: relative;
+    padding: 18px;
+    margin-bottom: 18px;
+    background-color: #fff;
+    border-bottom: 0 solid rgba(7, 17, 27, .1);
+    @include border-bottom-1px();
+
+    .desc {
+      padding-bottom: 18px;
+      border-bottom: 0 solid rgba(7, 17, 27, .1);
+      @include border-bottom-1px();
+
+      .name {
+        font-size: 14px;
+        line-height: 14px;
+        color: rgb(7, 17, 27);
+        margin-bottom: 8px;
+      }
+
+      .rating {
+        font-size: 0;
+      }
+
+      .star {
+        display: inline-block;
+        margin-right: 8px;
+        vertical-align: top;
+      }
+
+      .rating-count {
+        display: inline-block;
+        font-size: 10px;
+        line-height: 18px;
+        color: rgb(77, 85, 93);
+        margin-right: 12px;
+      }
+
+      .sell-count {
+        display: inline-block;
+        font-size: 10px;
+        line-height: 18px;
+        color: rgb(77, 85, 93);
+      }
+    }
+
+    .delivery {
+      display: flex;
+      padding-top: 18px;
+
+      .delivery-item {
+        flex: 1;
+        border-right: 0 solid rgba(7, 17, 27, .1);
+        /*.border-w(0, 1px, 0, 0);*/
+        text-align: center;
+
+        &:last-child {
+          border-right: none;
+        }
+
+        h1 {
+          font-size: 10px;
+          line-height: 10px;
+          color: rgb(147, 153, 159);
+          margin-bottom: 4px;
+        }
+
+        span {
+          font-size: 24px;
+          line-height: 24px;
+          color: rgb(7, 17, 27);
+        }
+
+        i {
+          font-style: normal;
+          font-size: 10px;
+        }
+      }
+    }
+
+    .favorite {
+      position: absolute;
+      top: 18px;
+      right: 18px;
+      width: 50px;
+      text-align: center;
+
+      .icon-favorite {
+        display: block;
+        margin-bottom: 4px;
+        line-height: 24px;
+        font-size: 24px;
+        color: #d4d6d9;
+
+        &.active {
+          color: rgb(240, 20, 20);
+        }
+      }
+
+      .text {
+        line-height: 10px;
+        font-size: 10px;
+        color: rgb(77, 85, 93);
+      }
+    }
+  }
+
+  .bulletin {
+    background-color: #fff;
+    border: 0 solid rgba(7, 17, 27, .1);
+    /*.border-w(1px, 0, 1px, 0);*/
+    .title {
+      padding: 18px 18px 0 18px;
+      font-size: 14px;
+      line-height: 14px;
+      color: rgb(7, 17, 27);
+    }
+
+    .content {
+      margin-top: 8px;
+      padding: 0 30px 16px 30px;
+      font-size: 12px;
+      line-height: 24px;
+      color: rgb(240, 20, 20);
+    }
+
+    .supports {
+      padding: 0 18px;
+
+      .support-item {
+        padding: 16px 12px;
+        border: 0 solid rgba(7, 17, 27, .1);
+        /*.border-w(1px, 0, 0, 0);*/
+        font-size: 0;
+
+        .icon {
+          display: inline-block;
+          vertical-align: top;
+          background-size: 16px 16px;
+          margin-right: 6px;
+          width: 16px;
+          height: 16px;
+
+          &.decrease {
+            @include bg-image('../../components/seller/decrease_4');
+          }
+
+          &.discount {
+            @include bg-image('../../components/seller/discount_4');
+          }
+
+          &.guarantee {
+            @include bg-image('../../components/seller/guarantee_4');
+          }
+
+          &.invoice {
+            @include bg-image('../../components/seller/invoice_4');
+          }
+
+          &.special {
+            @include bg-image('../../components/seller/special_4');
+          }
+        }
+
+        .text {
+          display: inline-block;
+          font-size: 12px;
+          line-height: 16px;
+          color: rgb(7, 17, 27);
+        }
+      }
+    }
+  }
+
+  .pics {
+    margin-top: 16px;
+    padding: 18px;
+    background-color: #fff;
+    border: 0 solid rgba(7, 17, 27, .1);
+    /*.border-w(1px, 0, 1px, 0);*/
+    .title {
+      margin-bottom: 12px;
+      line-height: 14px;
+      color: rgb(7, 17, 27);
+      font-size: 14px;
+    }
+
+    .pic-wrapper {
+      width: 100%;
+      overflow: hidden;
+      white-space: nowrap;
+
+      .pic-list {
+        font-size: 0;
+
+        .pic-item {
+          display: inline-block;
+          margin-right: 6px;
+          width: 120px;
+          height: 90px;
+
+          &:last-child {
+            margin: 0
+          }
+        }
+      }
+    }
+  }
+
+  .info {
+    padding: 18px 18px 0 18px;
+    color: rgb(7, 17, 27);
+    background-color: #fff;
+    margin-top: 16px;
+    border: 0 solid rgba(7, 17, 27, 0.1);
+    /*.border-w(1px, 0, 0, 0);*/
+    .title {
+      padding-bottom: 12px;
+      line-height: 14px;
+      border-bottom: 0 solid rgba(7, 17, 27, 0.1);
+      @include border-bottom-1px();
+      font-size: 14px;
+    }
+
+    .info-item {
+      padding: 16px 12px;
+      line-height: 16px;
+      font-size: 12px;
+      border-bottom: 0 solid rgba(7, 17, 27, 0.1);
+      @include border-bottom-1px();
+
+      &:last-child {
+        border: none;
+      }
+    }
   }
 </style>
